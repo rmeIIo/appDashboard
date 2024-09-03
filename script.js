06435e95d9b8d476505070e9106ad97a946a8df8
rmeiio <j.rogercorreamello@gmail.com>
2024-09-03 08:06:08 -0300

    Requisições para o back-end com load(), get() e post()

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(() => {
-	
-})
+  $("#documentacao").on("click", () => {
+    //$("#pagina").load("documentacao.html");
+
+    $.get("documentacao.html", data => {
+      $("#pagina").html(data);
+    })
+  });
+
+  $("#suporte").on("click", () => {
+    //$("#pagina").load("suporte.html");
+
+    $.post("suporte.html", data => {
+      $("#pagina").html(data);
+    })
+  });
+});
